0bd6dd7d79dd4a621d40e7d69837d8849f5c8145
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2017-11-27 00:41:47 +0900

    Update the plot script

--- aerial_manipulation/aerial_transportation/scripts/grasp_full_search_plot.py
+++ aerial_manipulation/aerial_transportation/scripts/grasp_full_search_plot.py
@@ -24,21 +24,29 @@ if __name__=="__main__":
     max_thrust_list = []
     min_thrust_list = []
 
+    valid_d_list = []
+    valid_delta_list = []
+    valid_state_list = []
+    valid_max_tau_list = []
+    valid_max_thrust_list = []
+    valid_min_thrust_list = []
+
+    v_valid_lower_bound_phi =  -0.221 #-0.224796
+    v_valid_upper_bound_phi =  -0.134 #-0.137796
+    v_valid_lower_bound_d = 0.102 #0.105
+    v_valid_upper_bound_d = 0.132 #0.135
+
     if object == "convex" :
         ax = Axes3D(fig)
-        ax.set_xlabel("d [m]")
-        ax.set_ylabel("delta [rad]")
-        ax.set_zlabel("norm of torque")
-        ax.set_xlim(0.1, 0.3)
-        ax.set_ylim(0.0, 0.6)
-        ax.set_zlim(2.0, 4.0)
+        #ax.set_xlabel("d [m]")
+        #ax.set_ylabel("delta [rad]")
+        #ax.set_zlabel("norm of torque")
 
     for line in open(file_name, 'r'):
 
         itemList = line.split('\t')
 
         if object == "convex" :
-            side = int(itemList[0])
             d = float(itemList[1])
             delta = float(itemList[2])
             state = float(itemList[3])
@@ -47,12 +55,20 @@ if __name__=="__main__":
             min_thrust = float(itemList[6])
 
             if max_tau != -1 :
-                d_list.append(d)
-                delta_list.append(delta)
-                state_list.append(state)
-                max_tau_list.append(max_tau)
-                max_thrust_list.append(max_thrust)
-                min_thrust_list.append(min_thrust)
+                if d > v_valid_lower_bound_d and d < v_valid_upper_bound_d and delta > v_valid_lower_bound_phi and delta < v_valid_upper_bound_phi:
+                    valid_d_list.append(d)
+                    valid_delta_list.append(delta)
+                    valid_state_list.append(state)
+                    valid_max_tau_list.append(max_tau)
+                    valid_max_thrust_list.append(max_thrust)
+                    valid_min_thrust_list.append(min_thrust)
+                else:
+                    d_list.append(d)
+                    delta_list.append(delta)
+                    state_list.append(state)
+                    max_tau_list.append(max_tau)
+                    max_thrust_list.append(max_thrust)
+                    min_thrust_list.append(min_thrust)
 
         if object == "cylinder" :
             delta = float(itemList[0])
@@ -68,22 +84,94 @@ if __name__=="__main__":
                 max_thrust_list.append(max_thrust)
                 min_thrust_list.append(min_thrust)
 
-    cm = pyplot.get_cmap("winter")
-    col = [cm(float(i)/(len(max_tau_list))) for i in xrange(0, len(max_tau_list))]
 
     if object == "convex" :
-        ax.scatter(d_list, delta_list, max_tau_list, s= 2, c=col)
+
+        ax.set_xlim(0.0, 0.2)
+        ax.set_ylim(-0.3, 0.2)
+        pyplot.tick_params(labelsize=18)
+
+        #cm = pyplot.get_cmap("winter")
+        #col = [cm(float(i)/(len(max_tau_list))) for i in xrange(0, len(max_tau_list))]
+
+        '''
+        # max_tau
+        ax.scatter(d_list, delta_list, max_tau_list, s= 1, alpha=0.5)
+        ax.scatter(valid_d_list, valid_delta_list, valid_max_tau_list, s= 1, color="red")
+        ax.set_zlim(2.0, 4.0)
+        max_value =3.09678
+        '''
+
+        '''
+        # min_thrust
+        ax.scatter(d_list, delta_list, min_thrust_list, s= 1, alpha=0.5)
+        ax.scatter(valid_d_list, valid_delta_list, valid_min_thrust_list, s= 1, color="red")
+        ax.set_zlim(9.7, 10.0)
+        max_value =9.883
+        '''
+
+        '''
+        # max_thrust
+        ax.scatter(d_list, delta_list, max_thrust_list, s= 1, alpha=0.5)
+        ax.scatter(valid_d_list, valid_delta_list, valid_max_thrust_list, s= 1, color="red")
+        ax.set_zlim(11.3, 11.55)
+        max_value =11.3885
+        '''
+
+        # state
+        ax.scatter(d_list, delta_list, state_list, s= 1, alpha=0.5)
+        ax.scatter(valid_d_list, valid_delta_list, valid_state_list, s= 1, color="red")
+        ax.set_zlim(12, 12.6)
+        max_value = 12.2
+
         start1 = []
         start2 = []
         start3 = []
-        start1.append(0.225)
-        start2.append(0.159704)
-        start3.append(3.09678)
+        start1.append(0.12)
+        start2.append(-0.181296)
+        start3.append(max_value)
 
-        ax.scatter(start1, start2, start3, s= 100, c='violet')
+        ax.scatter(start1, start2, start3, s= 150, c='black')
+        #ax.scatter(start1, start2, start3, s= 100, c='violet')
         pyplot.show()
 
     if object == "cylinder" :
-        pyplot.plot(delta_list, max_tau_list)
+        #fig, ax = pyplot.subplots()
+        #pyplot.xlabel("xlabel", fontsize=18)
+        #pyplot.ylabel("ylabel", fontsize=18)
+        pyplot.rcParams["font.size"] = 18
+        pyplot.axvspan(-0.0730052, 0.0139948, alpha=0.5, color='red')
+
+        '''
+        # max tau
+        y_min = min(max_tau_list) - 0.005
+        y_max = max(max_tau_list) + 0.005
+        pyplot.ylim((y_min,y_max))
+        pyplot.plot(delta_list, max_tau_list, color='black')
+        '''
+
+        '''
+        # min_thrust
+        y_min = min(min_thrust_list) - 0.005
+        y_max = max(min_thrust_list) + 0.005
+        pyplot.ylim((y_min,y_max))
+        pyplot.plot(delta_list, min_thrust_list, color='black')
+        '''
+
+        '''
+        # max_thrust
+        y_min = min(max_thrust_list) - 0.005
+        y_max = max(max_thrust_list) + 0.005
+        pyplot.ylim((y_min,y_max))
+        pyplot.plot(delta_list, max_thrust_list, color='black')
+        '''
+
+        # state
+        y_min = min(state_list) - 0.005
+        y_max = max(state_list) + 0.005
+        pyplot.ylim((y_min,y_max))
+        pyplot.plot(delta_list, state_list, color='black')
+
+        pyplot.vlines(x=-0.0295052, ymin=y_min, ymax=y_max, color='blue',  linestyles ='dashed', zorder=2)
         pyplot.show()
 
